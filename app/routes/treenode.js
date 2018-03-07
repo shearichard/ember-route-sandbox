d6f4cb2173a26324c955ad73a19c309ecccaaa78
Richard Shea <rshea@thecubagroup.com>
2018-03-07 21:29:59 +1300

    Now displaying a list of treenodes and responding to a button associated with each treenode object

--- app/routes/treenode.js
+++ app/routes/treenode.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return [{
-      id: 'Agrand-old-mansion',
+      id: 'grand-old-mansion',
     }, {
       id: 'Brban-living',
     }, {
---
+++ tests/unit/routes/treenode/housedetail-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:treenode/housedetail', 'Unit | Route | treenode/housedetail', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
