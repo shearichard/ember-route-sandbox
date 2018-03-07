4887112fc4ae7f56ec0011f1c6e300097cb9dae3
Richard Shea <rshea@thecubagroup.com>
2018-03-07 22:06:46 +1300

    Maintain state of most recently selected house via the treenode controller

--- app/controllers/treenode.js
+++ app/controllers/treenode.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
-
+const { computed } = Ember;
 export default Ember.Controller.extend({
+
+  lastSelectedHouse: -1,
+
+  lastSelectedHouseChanged: Ember.observer('lastSelectedHouse',function() {
+    console.log('lastSelectedHouse changed (' + this.get('lastSelectedHouse') + ')')
+  }),
+
   actions: {
     showDetail(id){
-      alert(id)
+      this.set('lastSelectedHouse', id);
     },
   }
 });
